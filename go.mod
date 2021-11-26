9d1cb177cbbc4c722f2fd5b6c436692fb690768a
hexaflex <github@hexaflex.nu>
2021-11-26 12:38:55 +0100

    Initial commit with full implementation.

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/hexaflex/wireworldgpu
+
+go 1.15
+
+require (
+	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2
+	github.com/hexaflex/pnm v0.0.0-20200810192450-e891a41ac33b
+	github.com/pkg/errors v0.9.1
+)
---
+++ version.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Application name and version constants.
+const (
+	AppVendor  = "hexaflex"
+	AppName    = "wireworldgpu"
+	AppVersion = "v0.0.2"
+)
+
+// Version returns a string with version information.
+func Version() string {
+	return fmt.Sprintf("%s %s %s",
+		AppVendor, AppName, AppVersion)
+}
